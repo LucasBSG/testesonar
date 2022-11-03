402ab0c622a1d4fbf0d9c062ba972c8d03672dc9
Lucas Camanducci <lcamanducci@gmail.com>
2022-11-03 16:57:05 -0300

    Encurtador adicionado

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,12 @@
 package com.example;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Hello world!
  *
@@ -10,4 +17,37 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    public static String shortURL(String urlToShort) {
+        URL url;
+        HttpURLConnection conn;
+        BufferedReader br = null;
+        String line;
+        try {
+            url = new URL("http://is.gd/create.php?format=simple&url="
+                    + urlToShort);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            br = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            if ((line = br.readLine()) != null) {
+                return line;
+            }
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+
+    
 }
